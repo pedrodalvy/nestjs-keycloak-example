a9af73f7e88f25563d5b7aa251997b3f7226f0c4
Pedro Dalvy <pedrodalvy@outlook.com>
2021-12-02 17:05:20 -0400

    configuracao do projeto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Resource, Roles, Unprotected } from 'nest-keycloak-connect';
 
 @Controller()
+@Resource('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Unprotected()
+  getPublic(): string {
+    return `${this.appService.getHello()} from public`;
+  }
+
+  @Get('/user')
+  @Roles({ roles: ['user'] })
+  getUser(): string {
+    return `${this.appService.getHello()} from user`;
+  }
+
+  @Get('/admin')
+  @Roles({ roles: ['admin'] })
+  getAdmin(): string {
+    return `${this.appService.getHello()} from admin`;
+  }
+
+  @Get('/all')
+  getAll(): string {
+    return `${this.appService.getHello()} from all`;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,54 @@
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+} from 'nest-keycloak-connect';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    // Criar um arquivo de configuração para o Keycloak no cenario ideal
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8081/auth',
+      realm: 'Nestjs',
+      clientId: 'nest-app',
+      secret: '3426ceff-7659-4746-8a81-4b30b9e45cb3',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // This adds a global level authentication guard,
+    // you can also have it scoped
+    // if you like.
+    //
+    // Will return a 401 unauthorized when it is unable to
+    // verify the JWT token or Bearer header is missing.
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    // This adds a global level resource guard, which is permissive.
+    // Only controllers annotated with @Resource and
+    // methods with @Scopes
+    // are handled by this guard.
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    // New in 1.1.0
+    // This adds a global level role guard, which is permissive.
+    // Used by `@Roles` decorator with the
+    // optional `@AllowAnyRole` decorator for allowing any
+    // specified role passed.
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
